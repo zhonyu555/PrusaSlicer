513f6da743e3a4dd2597ede6a373604b34d8956a
David F <kumpuu@web.de>
2019-10-20 15:36:53 +0200

    Dialog now smartly sets its height. Option category tree node added.

--- src/slic3r/GUI/UnsavedChangesDialog.cpp
+++ src/slic3r/GUI/UnsavedChangesDialog.cpp
@@ -4,11 +4,13 @@
 #include <boost/range/algorithm.hpp>
 
 #define UnsavedChangesDialog_max_width 600
+#define UnsavedChangesDialog_max_height 600
+#define UnsavedChangesDialog_min_height 200
 
 namespace Slic3r {
 	namespace GUI {
 		UnsavedChangesDialog::UnsavedChangesDialog(wxWindow* parent, GUI_App* app, const wxString& header, const wxString& caption, long style, const wxPoint& pos)
-			: wxDialog(parent, -1, caption, pos, wxSize(UnsavedChangesDialog_max_width,-1))
+			: wxDialog(parent, -1, caption, pos, wxSize(UnsavedChangesDialog_max_width, UnsavedChangesDialog_min_height))
 		{
 			m_app = app;
 
@@ -17,7 +19,7 @@ namespace Slic3r {
 			wxBoxSizer* main_sizer = new wxBoxSizer(wxVERTICAL);
 
 			wxString dirty_tabs;
-			wxScrolledWindow* scrolled_win = new wxScrolledWindow(this, wxID_ANY);
+			wxScrolledWindow* scrolled_win = new wxScrolledWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SIMPLE);
 
 				wxBoxSizer* scrolled_sizer = new wxBoxSizer(wxVERTICAL);
 
@@ -36,14 +38,11 @@ namespace Slic3r {
 						add_dirty_options(tab, scrolled_win, scrolled_sizer);
 					}
 				
-				scrolled_win->SetBackgroundColour(wxColour(255, 0, 0, 100));
-				scrolled_win->ShowScrollbars(wxScrollbarVisibility::wxSHOW_SB_ALWAYS, wxScrollbarVisibility::wxSHOW_SB_ALWAYS);
+				//scrolled_win->SetBackgroundColour(wxColour(255, 0, 0, 100));
+				//scrolled_win->ShowScrollbars(wxScrollbarVisibility::wxSHOW_SB_ALWAYS, wxScrollbarVisibility::wxSHOW_SB_ALWAYS);
 				scrolled_win->SetSizer(scrolled_sizer);
 
-				//scrolled_sizer->FitInside(scrolled_win);
-				//scrolled_win->FitInside();
 				scrolled_win->SetScrollRate(0, 2);
-				//scrolled_sizer->SetSizeHints(scrolled_win);
 
 			wxStaticText* msg = new wxStaticText(this, wxID_ANY, _(L("The presets on the following tabs were modified")) + ": " + dirty_tabs, wxDefaultPosition, wxDefaultSize);
 			wxFont msg_font = GUI::wxGetApp().bold_font();
@@ -59,7 +58,13 @@ namespace Slic3r {
 			main_sizer->Add(scrolled_win, 1, wxEXPAND | wxALL, 5);
 			main_sizer->Add(msg2, 0, wxALL, 5);
 			SetSizer(main_sizer);
-			//main_sizer->SetSizeHints(this);
+
+			this->Layout();
+			int x = scrolled_win->GetVirtualSize().y ;
+			int y = scrolled_win->GetSize().y;
+			int scrolled_add_height = scrolled_win->GetVirtualSize().y - scrolled_win->GetSize().y + 5;
+
+			this->SetSize(wxSize(UnsavedChangesDialog_max_width, std::min(UnsavedChangesDialog_min_height + scrolled_add_height, UnsavedChangesDialog_max_height)));
 		}
 
 		void UnsavedChangesDialog::add_dirty_options(Tab* tab, wxWindow* parent, wxBoxSizer* sizer) {
@@ -88,15 +93,46 @@ namespace Slic3r {
 
 			boost::sort(options);
 
+			std::string lastCat = "";
+			int left = 0;
 			for (def_opt_pair pair : options) {
+				std::string cat = pair.def->category;
+				std::string label = pair.def->label;
 				std::string old_val = pair.old_opt->serialize();
 				std::string new_val = pair.new_opt->serialize();
 
-				std::string msg = pair.def->category + "|" + pair.def->label + ": " + old_val + " -> " + new_val;
+				wxStaticText* line;
 
-				wxStaticText* opt = new wxStaticText(parent, wxID_ANY, msg, wxDefaultPosition, wxDefaultSize);
+				if (cat != "") {
+					if (cat != lastCat) {
+						sizer->Add(-1, 5);
+						line = new wxStaticText(parent, wxID_ANY, cat, wxDefaultPosition, wxDefaultSize);
+						line->SetFont(GUI::wxGetApp().bold_font());
+						sizer->Add(line, 0, wxLEFT | wxALIGN_LEFT, 15);
+					}
+					left = 25;
+				}
+				else {
+					left = 0;
+				}
+				
+				
 				sizer->Add(-1, 5);
-				sizer->Add(opt, 0, wxLEFT | wxALIGN_LEFT, 15);
+
+				wxBoxSizer* lineSizer = new wxBoxSizer(wxHORIZONTAL);
+
+					wxStaticText* opt_label = new wxStaticText(parent, wxID_ANY, label, wxDefaultPosition, wxSize(200,-1));
+					wxStaticText* opt_old = new wxStaticText(parent, wxID_ANY, old_val, wxDefaultPosition, wxDefaultSize);
+					wxStaticText* opt_new = new wxStaticText(parent, wxID_ANY, new_val, wxDefaultPosition, wxDefaultSize);
+					opt_new->SetForegroundColour(wxGetApp().get_label_clr_modified());
+
+					lineSizer->Add(opt_label);
+					lineSizer->AddStretchSpacer();
+					lineSizer->Add(opt_old);
+					lineSizer->AddStretchSpacer();
+					lineSizer->Add(opt_new, 0, wxRIGHT, 25);
+
+				sizer->Add(lineSizer, 0, wxLEFT | wxALIGN_LEFT | wxEXPAND, left);
 			}
 		}
 
