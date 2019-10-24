21c023f340eac76c424c6a0a12f02f71f8863c0f
David F <kumpuu@web.de>
2019-10-24 11:27:06 +0200

    Added save button. Added checkboxes to select what to save. Display coEnum.

--- src/slic3r/GUI/UnsavedChangesDialog.cpp
+++ src/slic3r/GUI/UnsavedChangesDialog.cpp
@@ -28,88 +28,80 @@ namespace Slic3r {
 			wxBoxSizer* main_sizer = new wxBoxSizer(wxVERTICAL);
 
 			wxString dirty_tabs;
-			wxScrolledWindow* scrolled_win = buildScrollWindow(dirty_tabs);
+			wxWindow* scrolled_win = buildScrollWindow(dirty_tabs);
 
 			wxStaticText* msg = new wxStaticText(this, wxID_ANY, _(L("The presets on the following tabs were modified")) + ": " + dirty_tabs, wxDefaultPosition, wxDefaultSize);
 				wxFont msg_font = GUI::wxGetApp().bold_font();
 				msg_font.SetPointSize(10);
 				msg->SetFont(msg_font);
-				msg->Wrap(UnsavedChangesDialog_max_width - 10);
 
-				wxBoxSizer* cont_stretch_sizer = new wxBoxSizer(wxHORIZONTAL);
-					wxPanel* cont_win = new wxPanel(this, wxID_ANY);
-					cont_win->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK));
-					wxBoxSizer* cont_sizer = new wxBoxSizer(wxHORIZONTAL);
-						wxStaticText* cont_label = new wxStaticText(cont_win, wxID_ANY, _(L("Continue? All unsaved changes will be discarded.")), wxDefaultPosition, wxDefaultSize);
-						wxButton* btn_yes = new wxButton(cont_win, wxID_ANY, "Yes");
-						btn_yes->Bind(wxEVT_BUTTON, ([this](wxCommandEvent& e) {
-							EndModal(wxID_YES);
-						}));
-
-						wxButton* btn_no = new wxButton(cont_win, wxID_ANY, "No");
-						btn_no->Bind(wxEVT_BUTTON, ([this](wxCommandEvent& e) {
-							EndModal(wxID_NO);
-						}));
-						btn_no->SetFocus();
-
-					cont_sizer->AddStretchSpacer();
-					cont_sizer->Add(cont_label, 0, wxALL | wxALIGN_CENTER_VERTICAL, 10);
-
-					cont_sizer->Add(btn_yes, 0, wxALIGN_CENTER_VERTICAL);
-					cont_sizer->Add(btn_no, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, UnsavedChangesDialog_def_border);
-
-					cont_win->SetSizer(cont_sizer);
-				cont_stretch_sizer->Add(cont_win, 1);
 
 			main_sizer->Add(msg, 0, wxALL, UnsavedChangesDialog_def_border);
-			main_sizer->Add(-1, 10);
+			main_sizer->Add(-1, UnsavedChangesDialog_def_border);
 			main_sizer->Add(scrolled_win, 1, wxEXPAND | wxALL, UnsavedChangesDialog_def_border);
-			main_sizer->Add(cont_stretch_sizer, 0, wxEXPAND);
+			main_sizer->Add(buildYesNoBtns(), 0, wxEXPAND | wxTOP, UnsavedChangesDialog_def_border * 2);
 			SetSizer(main_sizer);
 
 			this->Layout();
-			int scrolled_add_width = scrolled_win->GetVirtualSize().x - scrolled_win->GetSize().x + UnsavedChangesDialog_def_border;
-			int scrolled_add_height = scrolled_win->GetVirtualSize().y - scrolled_win->GetSize().y + UnsavedChangesDialog_def_border;
+			int scrolled_add_width = m_scroller->GetVirtualSize().x - m_scroller->GetSize().x + UnsavedChangesDialog_def_border;
 
-			this->SetSize(wxSize(std::min(UnsavedChangesDialog_min_width + scrolled_add_width, UnsavedChangesDialog_max_width), std::min(UnsavedChangesDialog_min_height + scrolled_add_height, UnsavedChangesDialog_max_height)));
+			int width = std::min(UnsavedChangesDialog_min_width + scrolled_add_width, UnsavedChangesDialog_max_width);
+			msg->Wrap(width - UnsavedChangesDialog_def_border * 2);
+
+			this->Layout();
+			int scrolled_add_height = m_scroller->GetVirtualSize().y - m_scroller->GetSize().y + UnsavedChangesDialog_def_border;
+			int height = std::min(UnsavedChangesDialog_min_height + scrolled_add_height, UnsavedChangesDialog_max_height);
+
+			this->SetSize(wxSize(width, height));
 		}
 
-		wxScrolledWindow* UnsavedChangesDialog::buildScrollWindow(wxString& dirty_tabs) {
-			wxScrolledWindow* scrolled_win = new wxScrolledWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SIMPLE);
+		wxWindow* UnsavedChangesDialog::buildScrollWindow(wxString& dirty_tabs) {
+			wxWindow* border_win = new wxWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SIMPLE);
+			border_win->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW));
+			wxBoxSizer* wrapper_sizer = new wxBoxSizer(wxVERTICAL);
 
-			wxBoxSizer* scrolled_sizer = new wxBoxSizer(wxVERTICAL);
+				wxScrolledWindow* scrolled_win = new wxScrolledWindow(border_win, wxID_ANY);
+				wxBoxSizer* scrolled_sizer = new wxBoxSizer(wxVERTICAL);
 
-			PrinterTechnology printer_technology = m_app->preset_bundle->printers.get_edited_preset().printer_technology();
-			bool highlight = false;
-			for (Tab* tab : m_app->tabs_list)
-				if (tab->supports_printer_technology(printer_technology) && tab->current_preset_is_dirty()) {
-					if (dirty_tabs.empty())
-						dirty_tabs = tab->title();
-					else
-						dirty_tabs += wxString(", ") + tab->title();
+				PrinterTechnology printer_technology = m_app->preset_bundle->printers.get_edited_preset().printer_technology();
+				bool highlight = false;
+				for (Tab* tab : m_app->tabs_list)
+					if (tab->supports_printer_technology(printer_technology) && tab->current_preset_is_dirty()) {
+						if (dirty_tabs.empty())
+							dirty_tabs = tab->title();
+						else
+							dirty_tabs += wxString(", ") + tab->title();
 
-					wxWindow* cur_tab_win = new wxWindow(scrolled_win, wxID_ANY);
-					wxBoxSizer* cur_tab_sizer = new wxBoxSizer(wxVERTICAL);
+						wxWindow* cur_tab_win = new wxWindow(scrolled_win, wxID_ANY);
+						wxBoxSizer* cur_tab_sizer = new wxBoxSizer(wxVERTICAL);
+							wxCheckBox* tabTitle = new wxCheckBox(cur_tab_win, wxID_ANY, tab->title(), wxDefaultPosition, wxDefaultSize);
+									tabTitle->SetFont(GUI::wxGetApp().bold_font());
 
-					wxStaticText* tabTitle = new wxStaticText(cur_tab_win, wxID_ANY, tab->title(), wxDefaultPosition, wxDefaultSize);
-						tabTitle->SetFont(GUI::wxGetApp().bold_font());
+							cur_tab_sizer->Add(tabTitle, 0, wxALL | wxALIGN_LEFT | wxALIGN_TOP, UnsavedChangesDialog_def_border);
+							add_dirty_options(tab, cur_tab_win, cur_tab_sizer);
 
-					cur_tab_sizer->Add(tabTitle, 0, wxALL | wxALIGN_LEFT | wxALIGN_TOP, UnsavedChangesDialog_def_border);
-					add_dirty_options(tab, cur_tab_win, cur_tab_sizer);
+							cur_tab_win->SetSizer(cur_tab_sizer);
 
-					cur_tab_win->SetSizer(cur_tab_sizer);
+							wxColour background = highlight ? wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK) : wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW);
+							highlight = !highlight;
+							cur_tab_win->SetBackgroundColour(background);
 
-					wxColour background = highlight ? wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK) : wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW);
-					highlight = !highlight;
-					cur_tab_win->SetBackgroundColour(background);
+						scrolled_sizer->Add(cur_tab_win, 0, wxEXPAND);
+					}
 
-					scrolled_sizer->Add(cur_tab_win, 1, wxEXPAND);
-				}
+				scrolled_win->SetSizer(scrolled_sizer);
+				scrolled_win->SetScrollRate(2, 2);
 
-			scrolled_win->SetSizer(scrolled_sizer);
-			scrolled_win->SetScrollRate(2, 2);
+			ScalableButton* save_btn = new ScalableButton(border_win, wxID_ANY, "save", _(L("Save selected")), wxDefaultSize, wxDefaultPosition, wxBU_LEFT | wxBU_EXACTFIT);
+			
+			wrapper_sizer->Add(scrolled_win, 1, wxEXPAND);
+			wrapper_sizer->AddSpacer(UnsavedChangesDialog_def_border * 2);
+			wrapper_sizer->Add(save_btn, 0, wxALL, UnsavedChangesDialog_def_border);
 
-			return scrolled_win;
+			border_win->SetSizer(wrapper_sizer);
+
+			this->m_scroller = scrolled_win;
+			return border_win;
 		}
 
 		void UnsavedChangesDialog::add_dirty_options(Tab* tab, wxWindow* parent, wxBoxSizer* sizer) {
@@ -149,16 +141,14 @@ namespace Slic3r {
 				const ConfigOption* old_opt = pair.old_opt;
 				const ConfigOption* new_opt = pair.new_opt;
 
-				wxStaticText* line;
-
 				if (cat != "") {
 					if (cat != lastCat) {
 						lastCat = cat;
 
 						sizer->Add(-1, UnsavedChangesDialog_def_border);
-						line = new wxStaticText(parent, wxID_ANY, cat, wxDefaultPosition, wxDefaultSize);
-						line->SetFont(GUI::wxGetApp().bold_font());
-						sizer->Add(line, 0, wxLEFT | wxALIGN_LEFT, UnsavedChangesDialog_def_border + UnsavedChangesDialog_child_indentation);
+						wxCheckBox* category = new wxCheckBox(parent, wxID_ANY, cat, wxDefaultPosition, wxDefaultSize);
+						category->SetFont(GUI::wxGetApp().bold_font());
+						sizer->Add(category, 0, wxLEFT | wxALIGN_LEFT, UnsavedChangesDialog_def_border + UnsavedChangesDialog_child_indentation);
 					}
 					left = UnsavedChangesDialog_def_border + UnsavedChangesDialog_child_indentation * 2;
 				}
@@ -170,7 +160,7 @@ namespace Slic3r {
 				sizer->Add(-1, UnsavedChangesDialog_def_border);
 
 				wxBoxSizer* lineSizer = new wxBoxSizer(wxHORIZONTAL);
-					wxStaticText* opt_label = new wxStaticText(parent, wxID_ANY, label, wxDefaultPosition, wxSize(200,-1));
+					wxCheckBox* opt_label = new wxCheckBox(parent, wxID_ANY, label, wxDefaultPosition, wxSize(200,-1));
 					wxWindow* win_old_opt;
 					wxWindow* win_new_opt;
 
@@ -200,7 +190,8 @@ namespace Slic3r {
 							case coInt:
 							case coInts:
 							case coBool:
-							case coBools:{
+							case coBools:
+							case coEnum:{
 								std::string old_val, new_val;
 								
 								old_val = old_opt->serialize();
@@ -327,6 +318,35 @@ namespace Slic3r {
 				_(L("parameter name")) + "\t: " + opt_id;
 
 			return tooltip;
+		}
+
+		wxBoxSizer* UnsavedChangesDialog::buildYesNoBtns() {
+			wxBoxSizer* cont_stretch_sizer = new wxBoxSizer(wxHORIZONTAL);
+			wxPanel* cont_win = new wxPanel(this, wxID_ANY);
+			cont_win->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK));
+			wxBoxSizer* cont_sizer = new wxBoxSizer(wxHORIZONTAL);
+			wxStaticText* cont_label = new wxStaticText(cont_win, wxID_ANY, _(L("Continue? All unsaved changes will be discarded.")), wxDefaultPosition, wxDefaultSize);
+			wxButton* btn_yes = new wxButton(cont_win, wxID_ANY, "Yes");
+			btn_yes->Bind(wxEVT_BUTTON, ([this](wxCommandEvent& e) {
+				EndModal(wxID_YES);
+			}));
+
+			wxButton* btn_no = new wxButton(cont_win, wxID_ANY, "No");
+			btn_no->Bind(wxEVT_BUTTON, ([this](wxCommandEvent& e) {
+				EndModal(wxID_NO);
+			}));
+			btn_no->SetFocus();
+
+			cont_sizer->AddStretchSpacer();
+			cont_sizer->Add(cont_label, 0, wxALL | wxALIGN_CENTER_VERTICAL, UnsavedChangesDialog_def_border * 3);
+
+			cont_sizer->Add(btn_yes, 0, wxALIGN_CENTER_VERTICAL);
+			cont_sizer->Add(btn_no, 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, UnsavedChangesDialog_def_border);
+
+			cont_win->SetSizer(cont_sizer);
+			cont_stretch_sizer->Add(cont_win, 1);
+
+			return cont_stretch_sizer;
 		}
 	}
 }
--- src/slic3r/GUI/UnsavedChangesDialog.hpp
+++ src/slic3r/GUI/UnsavedChangesDialog.hpp
@@ -12,9 +12,12 @@ namespace Slic3r {
 			UnsavedChangesDialog(wxWindow* parent, GUI_App* app, const wxString& header, const wxString& caption = wxMessageBoxCaptionStr, long style = wxOK | wxCENTRE, const wxPoint& pos = wxDefaultPosition);
 		private:
 			GUI_App* m_app;
-			wxScrolledWindow* buildScrollWindow(wxString& dirty_tabs);
+			wxScrolledWindow* m_scroller;
+
+			wxWindow* buildScrollWindow(wxString& dirty_tabs);
 			void add_dirty_options(Tab* tab, wxWindow* parent, wxBoxSizer* sizer);
 			std::string getTooltipText(const ConfigOptionDef& def);
+			wxBoxSizer* buildYesNoBtns();
 		};
 	}
 }
