3b050481f3952769c4c19062da3f6c82a16719ec
c1ebb <94982939+c1ebb@users.noreply.github.com>
2021-12-06 00:21:12 +0100

    remove %1% to get the preset type

--- src/slic3r/GUI/UnsavedChangesDialog.cpp
+++ src/slic3r/GUI/UnsavedChangesDialog.cpp
@@ -1555,7 +1555,7 @@ void DiffPresetDialog::update_bundles_from_app()
 
 void DiffPresetDialog::show(Preset::Type type /* = Preset::TYPE_INVALID*/)
 {
-    this->SetTitle(type == Preset::TYPE_INVALID ? _L("Compare Presets") : format_wxstr(_L("Compare %1% Presets"), wxGetApp().get_tab(type)->name()));
+    this->SetTitle(type == Preset::TYPE_INVALID ? _L("Compare Presets") : format_wxstr(_L("Compare Presets"), wxGetApp().get_tab(type)->name()));
     m_view_type = type;
 
     update_bundles_from_app();
