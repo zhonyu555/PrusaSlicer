f7545b55ad5439e101d374fe37a25ccc4aed9fe9
Robert Schiele <rschiele@gmail.com>
2023-04-16 13:05:48 +0200

    update TBB to v2021.9.0
    The old version by default built fat LTO objects causing false-positive
    diagnostics in anyways not used non-LTO build like this:
    
    In file included from /usr/lib64/gcc/x86_64-generic-linux/12/../../../../include/c++/12/atomic:41,
                     from /home/rschiele/oneTBB/src/tbb/../../include/oneapi/tbb/detail/_utils.h:22,
                     from /home/rschiele/oneTBB/src/tbb/address_waiter.cpp:17:
    In member function ‘std::__atomic_base<bool>::store(bool, std::memory_order)’,
        inlined from ‘std::atomic<bool>::store(bool, std::memory_order)’ at /usr/lib64/gcc/x86_64-generic-linux/12/../../../../include/c++/12/atomic:104:20,
        inlined from ‘tbb::detail::r1::concurrent_monitor_base<tbb::detail::r1::address_context>::abort_all_relaxed()’ at /home/rschiele/oneTBB/src/tbb/concurrent_monitor.h:430:53,
        inlined from ‘tbb::detail::r1::concurrent_monitor_base<tbb::detail::r1::address_context>::abort_all()’ at /home/rschiele/oneTBB/src/tbb/concurrent_monitor.h:413:26,
        inlined from ‘tbb::detail::r1::concurrent_monitor_base<tbb::detail::r1::address_context>::destroy()’ at /home/rschiele/oneTBB/src/tbb/concurrent_monitor.h:446:24,
        inlined from ‘tbb::detail::r1::clear_address_waiter_table()’ at /home/rschiele/oneTBB/src/tbb/address_waiter.cpp:60:40:
    /usr/lib64/gcc/x86_64-generic-linux/12/../../../../include/c++/12/bits/atomic_base.h:464:25: error: ‘__atomic_store_1’ writing 1 byte into a region of size 0 overflows the destination [-Werror=stringop-overflow=]
      464 |         __atomic_store_n(&_M_i, __i, int(__m));
          |         ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~
    compilation terminated due to -Wfatal-errors.
    cc1plus: all warnings being treated as errors
    
    The problem here was that for the non-LTO build (that will never be
    used) the compiler was warning about a potential risk that in the LTO
    build (that actually will be used) proved to not materialize.
    
    Alternatively for sure we could just have turned off generation of
    strict warning checks or turned off fat LTO object generation but since
    the latter is done by later tbb versions anyway I considered the upgrade
    to be the more straightforward solution.

--- deps/TBB/TBB.cmake
+++ deps/TBB/TBB.cmake
@@ -1,7 +1,7 @@
 prusaslicer_add_cmake_project(
     TBB
-    URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.5.0.zip"
-    URL_HASH SHA256=83ea786c964a384dd72534f9854b419716f412f9d43c0be88d41874763e7bb47
+    URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.9.0.zip"
+    URL_HASH SHA256=fcebb93cb9f7e882f62cd351b1c093dbefdcae04b616227dc716b0a5efa9e8ab
     CMAKE_ARGS          
         -DTBB_BUILD_SHARED=OFF
         -DTBB_TEST=OFF
