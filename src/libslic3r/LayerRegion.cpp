d4ccd28388a9f69f59a1b0cf8e34cbf43e8a8ca5
Christian Krafft <ckrafft@hackpad5>
2023-04-20 19:54:21 +0200

    ensure a reduction of bridge flows, if user disabled thick_bridges

--- src/libslic3r/LayerRegion.cpp
+++ src/libslic3r/LayerRegion.cpp
@@ -32,17 +32,24 @@ Flow LayerRegion::bridging_flow(FlowRole role, bool force_thick_bridges) const
     const PrintRegion       &region         = this->region();
     const PrintRegionConfig &region_config  = region.config();
     const PrintObject       &print_object   = *this->layer()->object();
-    if (print_object.config().thick_bridges || force_thick_bridges) {
-        // The old Slic3r way (different from all other slicers): Use rounded extrusions.
-        // Get the configured nozzle_diameter for the extruder associated to the flow role requested.
-        // Here this->extruder(role) - 1 may underflow to MAX_INT, but then the get_at() will follback to zero'th element, so everything is all right.
-        auto nozzle_diameter = float(print_object.print()->config().nozzle_diameter.get_at(region.extruder(role) - 1));
-        // Applies default bridge spacing.
-        return Flow::bridging_flow(float(sqrt(region_config.bridge_flow_ratio)) * nozzle_diameter, nozzle_diameter);
-    } else {
-        // The same way as other slicers: Use normal extrusions. Apply bridge_flow_ratio while maintaining the original spacing.
-        return this->flow(role).with_flow_ratio(region_config.bridge_flow_ratio);
-    }
+
+    // The old Slic3r way (different from all other slicers): Use rounded extrusions.
+    // Get the configured nozzle_diameter for the extruder associated to the flow role requested.
+    // Here this->extruder(role) - 1 may underflow to MAX_INT, but then the get_at() will follback to zero'th element, so everything is all right.
+    const auto nozzle_diameter = float(print_object.print()->config().nozzle_diameter.get_at(region.extruder(role) - 1));
+    // Applies default bridge spacing.
+    const auto round_flow = Flow::bridging_flow(float(sqrt(region_config.bridge_flow_ratio)) * nozzle_diameter, nozzle_diameter);
+
+    // The same way as other slicers: Use normal extrusions. Apply bridge_flow_ratio while maintaining the original spacing.
+    const auto normal_flow = this->flow(role).with_flow_ratio(region_config.bridge_flow_ratio);
+
+    // If extrusion_width * layer_height > nozzle_diameter ^ 2 * Pi/4, "normal" flow can get much higher than the rounded flow
+    // In that case, bridges based on normal_flow would become thicker (and unusable) than with round_flow
+    // So, return rounded_flow to ensure that bridges are indeed thinner thick_bridges is disabled
+    if ((print_object.config().thick_bridges || force_thick_bridges) && (round_flow.mm3_per_mm() > normal_flow.mm3_per_mm()))
+        return round_flow;
+    else
+        return normal_flow;
 }
 
 // Fill in layerm->fill_surfaces by trimming the layerm->slices by layerm->fill_expolygons.
