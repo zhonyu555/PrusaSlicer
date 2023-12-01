7f39465802354fdd695df636047e07796522bc8a
Seth LaForge <sethml@ofb.net>
2023-11-30 20:26:01 -0800

    Delete debugging and clean up comments.

--- src/libslic3r/Arachne/utils/ExtrusionLine.cpp
+++ src/libslic3r/Arachne/utils/ExtrusionLine.cpp
@@ -44,14 +44,6 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
     if (junctions.size() <= min_path_size)
         return;
 
-    printf("BEFORE ExtrusionLine::simplify on %zu points:\n", junctions.size());
-    for (size_t point_idx = 0; point_idx < junctions.size(); point_idx++) {
-        const ExtrusionJunction &cur = junctions[point_idx];
-        const ExtrusionJunction &prev = point_idx ? junctions[point_idx - 1] : cur;
-        printf("  %zu: %.3f, %.3f, w %.3f, len %.3f\n", point_idx, unscaled(cur.p[0]), unscaled(cur.p[1]),
-               unscaled(cur.w), unscaled(cur - prev).norm());
-    }
-
     /* ExtrusionLines are treated as (open) polylines, so in case an ExtrusionLine is actually a closed polygon, its
      * starting and ending points will be equal (or almost equal). Therefore, the simplification of the ExtrusionLine
      * should not touch the first and last points. As a result, start simplifying from point at index 1.
@@ -104,7 +96,6 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
         if (length2 < scaled<coord_t>(0.025))
         {
             // We're allowed to always delete segments of less than 5 micron. The width in this case doesn't matter that much.
-            printf("  Too short: %zu len %.3f\n", point_idx, unscaled(sqrt(length2)));
             continue;
         }
 
@@ -113,7 +104,6 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
 
         if (base_length_2 == 0) // Two line segments form a line back and forth with no area.
         {
-            printf("  Base length zero: %zu, len %.3f\n", point_idx, unscaled(sqrt(length2)));
             continue; // Remove the junction (vertex).
         }
         //We want to check if the height of the triangle formed by previous, current and next vertices is less than allowed_error_distance_squared.
@@ -130,8 +120,6 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
             // We shouldn't remove middle junctions of colinear segments if the area changed for the C-P segment is exceeding the maximum allowed
              && extrusion_area_error <= maximum_extrusion_area_deviation)
         {
-            printf("  Colinear: %zu, height %.3f, area error %.3f < %.3f\n", point_idx, unscaled(sqrt(height_2)),
-                   unscaled(sqrt(extrusion_area_error)), unscaled(sqrt(maximum_extrusion_area_deviation)));
             // Remove the current junction (vertex).
             continue;
         }
@@ -146,26 +134,19 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
                 // We should instead move this point to a location where both edges are kept and then remove the previous point that we wanted to keep.
                 // By taking the intersection of these two lines, we get a point that preserves the direction (so it makes the corner a bit more pointy).
                 // We just need to be sure that the intersection point does not introduce an artifact itself.
-                //      o  prev_prev
-                //      |
-                //      o  prev
-                //       \  cur
-                // inte + o------------o next
-                //
+                //                o < prev_prev
+                //                |
+                //                o < prev
+                //                  \  < short segment
+                // intersection > +   o-------------------o < next
+                //                    ^ current
                 Point intersection_point;
                 bool has_intersection = Line(previous_previous.p, previous.p).intersection_infinite(Line(current.p, next.p), &intersection_point);
                 if (!has_intersection
                     || Line::distance_to_infinite_squared(intersection_point, previous.p, current.p) > double(allowed_error_distance_squared)
                     || (intersection_point - previous.p).cast<int64_t>().squaredNorm() > smallest_line_segment_squared  // The intersection point is way too far from the 'previous'
-                    || (intersection_point - current.p).cast<int64_t>().squaredNorm() > smallest_line_segment_squared)     // and 'current' points, so it shouldn't replace 'current'
+                    || (intersection_point - current.p).cast<int64_t>().squaredNorm() > smallest_line_segment_squared)  // and 'current' points, so it shouldn't replace 'current'
                 {
-                    printf("  NO BETTER SPOT: %zu, length %.3f < %.3f, height %.3f < %.3f, intersect %d %.3f, %.3f, len %.3f, %.3f, %.3f\n", point_idx,
-                           unscaled(sqrt(length2)), unscaled(sqrt(smallest_line_segment_squared)),
-                           unscaled(sqrt(height_2)), unscaled(sqrt(allowed_error_distance_squared)),
-                           has_intersection, unscaled(intersection_point.x()), unscaled(intersection_point.y()),
-                           unscaled((intersection_point - previous.p).cast<int64_t>().norm()),
-                           unscaled((intersection_point - current.p).cast<int64_t>().norm()),
-                           unscaled((intersection_point - next.p).cast<int64_t>().norm()));
                     // We can't find a better spot for it, but the size of the line is more than 5 micron.
                     // So the only thing we can do here is leave it in...
                 }
@@ -184,16 +165,12 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
                     accumulated_area_removed = removed_area_next; // So that in the next iteration it's the area between the origin, [previous] and [current]
                     previous_previous = previous;
                     previous = new_to_add; // Note that "previous" is only updated if we don't remove the junction (vertex).
-                    printf("  Replaced %zu with: %zu, at %.3f, %.3f\n", new_junctions.size(), point_idx,
-                           unscaled(intersection_point.x()), unscaled(intersection_point.y()));
                     new_junctions.push_back(new_to_add);
                     continue;
                 }
             }
             else
             {
-                printf("  Next segment small: %zu, next %.3f < %.3f\n", point_idx,
-                       unscaled(sqrt(next_length2)), unscaled(sqrt(4 * smallest_line_segment_squared)));
                 continue; // Remove the junction (vertex).
             }
         }
@@ -201,7 +178,6 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
         accumulated_area_removed = removed_area_next; // So that in the next iteration it's the area between the origin, [previous] and [current]
         previous_previous = previous;
         previous = current; // Note that "previous" is only updated if we don't remove the junction (vertex).
-        printf("  %zu => %zu\n", point_idx, new_junctions.size());
         new_junctions.push_back(current);
     }
 
@@ -216,14 +192,6 @@ void ExtrusionLine::simplify(const int64_t smallest_line_segment_squared, const 
     }
 
     junctions = new_junctions;
-
-    printf("AFTER ExtrusionLine::simplify on %zu points:\n", junctions.size());
-    for (size_t point_idx = 0; point_idx < junctions.size(); point_idx++) {
-        const ExtrusionJunction &cur = junctions[point_idx];
-        const ExtrusionJunction &prev = point_idx ? junctions[point_idx - 1] : cur;
-        printf("  %zu: %.3f, %.3f, w %.3f, len %.3f\n", point_idx, unscaled(cur.p[0]), unscaled(cur.p[1]),
-               unscaled(cur.w), unscaled(cur - prev).norm());
-    }
 }
 
 int64_t ExtrusionLine::calculateExtrusionAreaDeviationError(ExtrusionJunction A, ExtrusionJunction B, ExtrusionJunction C) {
