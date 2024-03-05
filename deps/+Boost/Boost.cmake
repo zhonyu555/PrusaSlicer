8d7701cb76b177d44bda4a804cd30e98591a3f09
Hernan Martinez <hernan.c.martinez@gmail.com>
2024-03-05 06:53:50 -0700

    Boost update to 1.84 and use Fibers as Boost.Context implementation on MSVC/ARM64

--- deps/+Boost/Boost.cmake
+++ deps/+Boost/Boost.cmake
@@ -1,6 +1,7 @@
 
 set(_context_abi_line "")
 set(_context_arch_line "")
+set(_context_implementation_line "-DBOOST_CONTEXT_IMPLEMENTATION:STRING=fcontext")
 if (APPLE AND CMAKE_OSX_ARCHITECTURES)
     if (CMAKE_OSX_ARCHITECTURES MATCHES "x86")
         set(_context_abi_line "-DBOOST_CONTEXT_ABI:STRING=sysv")
@@ -10,9 +11,13 @@ if (APPLE AND CMAKE_OSX_ARCHITECTURES)
     set(_context_arch_line "-DBOOST_CONTEXT_ARCHITECTURE:STRING=${CMAKE_OSX_ARCHITECTURES}")
 endif ()
 
+if (MSVC AND CMAKE_SYSTEM_PROCESSOR MATCHES "^(ARM64|aarch64)$")
+    set(_context_implementation_line "-DBOOST_CONTEXT_IMPLEMENTATION=winfib")
+endif()
+
 add_cmake_project(Boost
-    URL "https://github.com/boostorg/boost/releases/download/boost-1.83.0/boost-1.83.0.zip"
-    URL_HASH SHA256=9effa3d7f9d92b8e33e2b41d82f4358f97ff7c588d5918720339f2b254d914c6
+    URL "https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.zip"
+    URL_HASH SHA256=f46e9a747e0828130d37ead82b796ab82348e3a7ee688cd43b6c5f35f5e71aef
     LIST_SEPARATOR |
     CMAKE_ARGS
         -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test
@@ -20,6 +25,7 @@ add_cmake_project(Boost
         -DBUILD_TESTING:BOOL=OFF
         "${_context_abi_line}"
         "${_context_arch_line}"
+        "${_context_implementation_line}"
 )
 
 set(DEP_Boost_DEPENDS ZLIB)
