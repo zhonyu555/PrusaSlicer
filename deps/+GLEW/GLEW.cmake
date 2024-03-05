507cd1a63856ba4a4d33e8ed1c08ac1236b99035
Hernan Martinez <hernan.c.martinez@gmail.com>
2024-03-05 04:42:36 -0700

    GLEW: Set base address only in 32bit
    See https://github.com/nigels-com/glew/pull/384

--- deps/+GLEW/GLEW.cmake
+++ deps/+GLEW/GLEW.cmake
@@ -3,6 +3,7 @@ add_cmake_project(
   URL https://sourceforge.net/projects/glew/files/glew/2.2.0/glew-2.2.0.zip
   URL_HASH SHA256=a9046a913774395a095edcc0b0ac2d81c3aacca61787b39839b941e9be14e0d4
   SOURCE_SUBDIR build/cmake
+  PATCH_COMMAND COMMAND ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/GLEW.patch
   CMAKE_ARGS
     -DBUILD_UTILS=OFF
 )
