385e24bfa2dcd9cf644ddcbaa0a7863e4f12d72a
rhradec <me@hradec.com>
2024-03-20 17:26:34 -0700

    Added a docker build script to easily build Ubuntu 20.04 binaries using docker on any Linux distro.
    just run the script with docker installed, and you should get a "prusa-slicer" executable file at the root of the git folder. Just execute it and prusa-slicer should run, as long as your linux distro is newer than Ubuntu 20.04.

---
+++ docker/build-for-ubuntu-20.04-using-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CD=$(dirname $(readlink -f $0))
+cd "$CD"
+
+docker build . -t prusa-slicer && \
+docker rm -f prusa-build 2>/dev/null && \
+docker run --rm -ti --name prusa-build \
+	-v "$CD/../":/data/ \
+	prusa-slicer \
+/bin/bash -c "	cd /data/deps && \
+		mkdir -p build && \
+		cd build && \
+		cmake .. -DDEP_WX_GTK3=ON && \
+		(make -j 8 || /bin/bash) && \
+		cd /data/ && \
+		mkdir -p build && \
+		cd build && \
+		cmake .. -DSLIC3R_STATIC=1 -DSLIC3R_GTK=3 -DSLIC3R_PCH=OFF -DCMAKE_PREFIX_PATH=/data/deps/build/destdir/usr/local -DSLIC3R_GUI=1 -DSLIC3R_STATIC=1  && \
+		make -j 8 && \
+		cp /data/build/src/prusa-slicer /data/ \
+"
