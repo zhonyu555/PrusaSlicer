587b63b9108fc4c84d2b667265e2a19289dd8594
themanyone <nospam@thenerdshow.com>
2024-07-07 10:56:29 -0800

    do not activate search_dialog on linux

--- src/slic3r/GUI/Search.cpp
+++ src/slic3r/GUI/Search.cpp
@@ -656,7 +656,7 @@ void SearchDialog::Popup(wxPoint position /*= wxDefaultPosition*/)
 
     if (position != wxDefaultPosition)
         this->SetPosition(position);
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__linux__)
     this->ShowWithoutActivating();
 #else
     this->Show();
