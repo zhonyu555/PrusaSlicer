e20617c4d789621c050b994623564184a90a5b31
Hernan Martinez <hernan.c.martinez@gmail.com>
2024-07-16 20:58:51 -0600

    Boost: Use Windows Fibers as Boost.Context implementation on MSVC/ARM64

--- deps/+Boost/Boost.cmake
+++ deps/+Boost/Boost.cmake
@@ -1,6 +1,7 @@
 
 set(_context_abi_line "")
 set(_context_arch_line "")
+set(_context_implementation_line "-DBOOST_CONTEXT_IMPLEMENTATION:STRING=fcontext")
 if (APPLE AND CMAKE_OSX_ARCHITECTURES)
     if (CMAKE_OSX_ARCHITECTURES MATCHES "x86")
         set(_context_abi_line "-DBOOST_CONTEXT_ABI:STRING=sysv")
@@ -9,6 +10,10 @@ if (APPLE AND CMAKE_OSX_ARCHITECTURES)
     endif ()
     set(_context_arch_line "-DBOOST_CONTEXT_ARCHITECTURE:STRING=${CMAKE_OSX_ARCHITECTURES}")
 endif ()
+
+if (MSVC AND CMAKE_SYSTEM_PROCESSOR MATCHES "^(ARM64|aarch64)$")
+    set(_context_implementation_line "-DBOOST_CONTEXT_IMPLEMENTATION=winfib")
+endif()
 
 add_cmake_project(Boost
     URL "https://github.com/boostorg/boost/releases/download/boost-1.83.0/boost-1.83.0.zip"
@@ -20,6 +25,7 @@ add_cmake_project(Boost
         -DBUILD_TESTING:BOOL=OFF
         "${_context_abi_line}"
         "${_context_arch_line}"
+        "${_context_implementation_line}"
 )
 
 set(DEP_Boost_DEPENDS ZLIB)
